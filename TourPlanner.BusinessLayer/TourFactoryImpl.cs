26fdc255be5c184103ba5acf93a793083db3280e
misternic <50358374+misternic@users.noreply.github.com>
2022-03-08 19:23:41 +0100

    added second BusinessLayer as WPF Class Library

---
+++ TourPlanner.BusinessLayer/TourFactoryImpl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TourPlanner.BusinessLayer
+{
+    internal class TourFactoryImpl : ITourFactory
+    {
+        public IEnumerable<Tour> GetItems()
+        {
+            return new List<Tour>()
+            {
+                new Tour() { Name = "Tour Item 1" },
+                new Tour() { Name = "Tour Item 2" },
+                new Tour() { Name = "Tour Item 3" },
+                new Tour() { Name = "Tour Item 4" }
+            };
+        }
+
+        public IEnumerable<Tour> Search(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
